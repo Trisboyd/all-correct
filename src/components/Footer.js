468b55f67c2e75e5e02476d5474cdd4c96cce798
Tristan Boyd <trisboyd@gmail.com>
2021-09-28 10:59:25 -0600

    'fixed formatting of bottom row and link hrefs'

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,7 +3,7 @@ import Navigation from './Navigation';
 import facebook from "../images/facebook.png";
 import linkedIn from '../images/linkedin.png';
 import medium from '../images/medium.png';
-import rss from '../images/Vector.png';
+import rss from '../images/Group.png';
 
 export default function Footer() {
 
@@ -23,34 +23,33 @@ export default function Footer() {
                         Ireland W23 V3P2</p>
                     <p className='footer__text'>CANADA: <br />
                         119 Spadina Ave, Toronto, ON M6E3J3</p>
-                    <p className='footer__text'>© Allcorrect Group 2006—2021, «Legal information»</p>
+                    <p className='footer__text footer__text_copyright'>© Allcorrect Group 2006—2021, «Legal information»
+                    <span className='footer__text_website'>site@allcorrectgames.com</span></p>
                 </div>
-                <p className='footer__text footer__text_website'>site@allcorrectgames.com</p>
                 <ul className='footer__links'>
                     <li className='footer__item'>
-                        <a className='footer__link' href=''>
+                        <a className='footer__link' href='https://www.facebook.com/allcorrectgames'>
                             <img className='footer__image'
                             src={facebook}
-                            alt='social'></img>Facebook</a>
+                            alt='facebook'></img>Facebook</a>
                     </li>
                     <li className='footer__item'>
-                        <a className='footer__link' href=''>
+                        <a className='footer__link' href='https://medium.com/@allcorrect'>
                             <img className='footer__image' 
                             src={medium}
-                            alt='social-icon'></img>Medium</a>
+                            alt='medium'></img>Medium</a>
                     </li>
                     <li className='footer__item'>
-                        <a className='footer__link' href=''>
+                        <a className='footer__link' href='https://www.linkedin.com/company/allcorrectgames?originalSubdomain=ie'>
                             <img className='footer__image'
                             src={linkedIn} 
-                            alt='social-icon'></img>LinkedIn</a>
+                            alt='linkedin'></img>LinkedIn</a>
                     </li>
                     <li className='footer__item'>
                         <a className='footer__link' href=''>
-                            <img className='footer__image'
+                            <img className='footer__image footer__image_white'
                             src={rss} 
-                            alt='social-icon'></img> 
-                            RSS</a>
+                            alt='rss'></img>RSS</a>
                     </li>
                 </ul>
             </div>
